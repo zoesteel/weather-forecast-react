d4631963c3c5dc90fdc80dad4d64cd5e4b2f4842
zoesteel <zoeysteel@gmail.com>
2019-08-23 13:57:06 -0700

    Updates

---
+++ precache-manifest.7bfcec3eafbae6974a72372ca7b8a8f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb9315293a5d6e90b188fd618e168454",
+    "url": "/weather-forecast-react/index.html"
+  },
+  {
+    "revision": "2aa60ae5669b009337e7",
+    "url": "/weather-forecast-react/static/css/main.1757e1cc.chunk.css"
+  },
+  {
+    "revision": "386c48c68f1bddd1badb",
+    "url": "/weather-forecast-react/static/js/2.d8b431e5.chunk.js"
+  },
+  {
+    "revision": "2aa60ae5669b009337e7",
+    "url": "/weather-forecast-react/static/js/main.1a5a8fb3.chunk.js"
+  },
+  {
+    "revision": "a652a8e3cda5f77661a6",
+    "url": "/weather-forecast-react/static/js/runtime~main.d324ba79.js"
+  },
+  {
+    "revision": "7908f14719f208627a181b99f8becad2",
+    "url": "/weather-forecast-react/static/media/sun.7908f147.png"
+  }
+]);
